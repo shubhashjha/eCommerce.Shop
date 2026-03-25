4311e261e0508d73dbda9e09dce4aa16ab972c17
shubhashjha <shubhash.xcode@gmail.com>
2026-03-25 21:58:35 +0530

    Initial commit

---
+++ eCommerce.CouponService/Model/Coupon.cs
@@ -0,0 +1,12 @@
+﻿namespace eCommerce.CouponService.Model
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public decimal DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ eCommerce.CouponService/Model/DTO/CouponDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace eCommerce.CouponService.Model.DTO
+{
+    public class CouponDTO
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public decimal DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
