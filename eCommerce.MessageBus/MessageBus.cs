8ae32f1d0fc3391c1b91bce3e407283c918ed72d
shubhashjha <shubhash.xcode@gmail.com>
2026-04-14 23:05:18 +0530

    Replace Connection string

--- eCommerce.MessageBus/MessageBus.cs
+++ eCommerce.MessageBus/MessageBus.cs
@@ -7,8 +7,8 @@ namespace eCommerce.MessageBus
     public class MessageBus : IMessageBus
     {
 
-        private string connectionString = "Endpoint=sb://eCommerce.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=HjoslS58pPHtAULb0tay/jx4Ys0+MO5/R+ASbCcFTG0=";
-
+        private string connectionString = "CONNECTION_STRING";
+        
         public async Task PublishMessage(object message, string topic_queue_Name)
         {
             await using var client = new ServiceBusClient(connectionString);
